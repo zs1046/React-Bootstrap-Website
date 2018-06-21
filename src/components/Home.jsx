f408ea46fd4900fda1d2be055556feb110965645
zs1046 <zachsotak@yahoo.com>
2018-06-21 10:49:32 -0500

    fix pictures

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,17 +16,17 @@ export default class Home extends Component {
                 </Jumbotron>
                 <Row className="show-grid text-center">
                     <Col xs={12} sm={4} className="person-wrapper">
-                        <Image src="assets/person-1.jpg" circle className="profile-pic"/>
+                        <Image src="../public/assets/person-1.jpg" circle className="profile-pic"/>
                         <h3>Frank</h3>
                         <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
                     </Col>
                     <Col xs={12} sm={4} className="person-wrapper">
-                        <Image src="assets/person-2.jpg" circle className="profile-pic"/>
+                        <Image src="../public/assets/person-2.jpg" circle className="profile-pic"/>
                         <h3>Vanessa</h3>
                         <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
                     </Col>
                     <Col xs={12} sm={4} className="person-wrapper">
-                        <Image src="assets/person-3.jpg" circle className="profile-pic"/>
+                        <Image src="../public/assets/person-3.jpg" circle className="profile-pic"/>
                         <h3>Riff</h3>
                         <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
                     </Col>
